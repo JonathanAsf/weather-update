10686363d114e28c6351722b50f8aec9f9445469
Jonathan Almeida <90427766+JonathanAsf@users.noreply.github.com>
2022-03-18 01:59:41 -0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,12 +1,13 @@
-import request 
+#unfinished
+import requests 
 from bs4 import BeautifulSoup
 
-search="Weather in São Paulo"
+search="Clima em São Palo"
 
 url=f"https://www.climatempo.com.br/previsao-do-tempo/15-dias/cidade/558/saopaulo-sp"
 
 r=requeust.get(url)
 s=BeautifulSop(r.text,"html.parser")
 
-update=s.find("div"class_="BNeawe").text
+update=s.find("div",class_="BNeawe").text
 print(update)
